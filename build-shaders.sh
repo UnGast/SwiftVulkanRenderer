f5c8ba71f50a9b09eea6d025d4f6438872ed30c1
Adrian Zimmermann <zadrian@web.de>
2021-06-20 11:21:40 +0200

    lower necessary spv version

--- build-shaders.sh
+++ build-shaders.sh
@@ -1,3 +1,3 @@
 export DIR=Sources/SwiftVulkanRenderer/Resources
-glslc --target-spv=spv1.5 -fshader-stage=vert ${DIR}/vertex.glsl -o ${DIR}/vertex.spv
-glslc --target-spv=spv1.5 -fshader-stage=frag ${DIR}/fragment.glsl -o ${DIR}/fragment.spv
+glslc --target-spv=spv1.3 -fshader-stage=vert ${DIR}/vertex.glsl -o ${DIR}/vertex.spv
+glslc --target-spv=spv1.3 -fshader-stage=frag ${DIR}/fragment.glsl -o ${DIR}/fragment.spv
